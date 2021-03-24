6ed44448fcc1d9cff6ea35e1550e4d2d7815b888
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-03-24 14:12:25 +0700

    update: added example navigating with the link component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Link from 'next/link';
 const HomePage = () => {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+      </ul>
     </div>
   );
 };
