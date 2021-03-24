ebafcef294ed2ed66aa0d6739c8e3b4ddd94ef8e
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-03-24 14:38:12 +0700

    update: added example redirect navigation{

--- pages/client/[id]/index.js
+++ pages/client/[id]/index.js
@@ -4,9 +4,23 @@ function ClientProjectsPage() {
 
   console.log(router.query);
 
+  function loadProjectHandler() {
+    // ...load data
+
+    // Way 1
+    // router.push('/client/muh/project-a');
+
+    // Way 2
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'muh', clientprojectid: 'project-a' },
+    });
+  }
+
   return (
     <div>
       <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
